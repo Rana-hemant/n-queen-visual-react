361cf2568f9a508cd3133c7053999fbf883f17ba
Hemant Rana <113758055+Rana-hemant@users.noreply.github.com>
2023-07-25 00:44:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import Chessboard from "./Chessboard.js";
+const App = () => {
+  return (
+    <div>
+      <Chessboard />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer(){
+  return <footer>Created by <a href="https://github.com/Rana-hemant?tab=repositories">Ranahemant</a></footer>
+}
+
+export default Footer;
